4e8b854a19e8947df5f3e4ee0ab414249b7dad24
Dalton Knuckles <djck244@gmail.com>
2023-05-21 18:52:52 -0700

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 
 function App() {
@@ -24,7 +31,8 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
